9c029d3148b3e8a56b02f19ed964d189ca7db7ad
huangheshuai <1692717842@qq.com>
2019-05-31 12:27:32 +0800

    Python 高级编程

--- chapter_04/4_2.py
+++ chapter_04/4_2.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import abc
+
 from collections.abc import Sized
 
 """
@@ -16,12 +18,42 @@ class Company(object):
 
     def __getitem__(self, item):
         """
-            描述：只需要申明该函数，该类就是集合、序列
+            描述：
+                只需要申明该函数，该类就是集合、序列
         """
         return self.employee[item]
 
     def __len__(self):
         return len(self.employee)
+
+
+class CacheBase(metaclass=abc.ABCMeta):
+    """
+        描述：
+            实现抽象基类(协议或者约定)
+    """
+    @abc.abstractmethod
+    def get(self, key):
+        pass
+
+    @abc.abstractmethod
+    def set(self, key, value):
+        pass
+
+
+class RedisCache(CacheBase):
+    """
+        描述：
+            实现 redis 缓存
+        备注：
+            RedisCache 对象继承自 CacheBase，而 CacheBase 是抽象基类，所以 RedisCache 必须实现 CacheBase 里的所有方法, 否则报错：
+            TypeError: Can't instantiate abstract class RedisCache with abstract methods get
+    """
+    def set(self, key, value):
+        pass
+
+    def get(self, key):
+        pass
 
 
 if __name__ == '__main__':
@@ -35,6 +67,9 @@ if __name__ == '__main__':
     # 方法二 使用抽象基类来判断
     length = isinstance(com, Sized)
     print(length)
+
+    redis_cache = RedisCache()
+    redis_cache.set('key', 'value')
 
 
 
